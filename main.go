4279575dff8da277d455977b4de3164b15143428
iankendoit <iankencruz@gmail.com>
2023-09-10 16:13:53 +1000

    update

--- main.go
+++ main.go
@@ -3,28 +3,14 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
-func RunServer() {
-	r := gin.Default()
-	r.GET("/", homeHandler)
-	r.GET("/user", getUser)
-	r.Run()
-}
-
 func main() {
-	RunServer()
-}
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)
+	http.ListenAndServe(":8080", router)
 
-func homeHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Home",
-	})
-}
-
-func getUser(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"User": "Ian",
-	})
 }
